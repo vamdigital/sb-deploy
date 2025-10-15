ea2ab0c1e3555f634664fdf0ce259266dee2db48
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2025-10-15 13:15:45 +0100

    fix(feat-1) - trying to see if it works

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -29,26 +29,26 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button Feat-1',
+    label: 'Button Feat-1.1',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button Feat-1',
+    label: 'Button Feat-1.1',
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-    label: 'Button Feat-1',
+    label: 'Button Feat-1.1',
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    label: 'Button Feat-1',
+    label: 'Button Feat-1.1',
   },
 };

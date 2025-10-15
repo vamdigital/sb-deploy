779c4ea4772cdd92eebb62213c440abdd90d6766
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2025-10-15 13:33:24 +0100

    (feat-2) - new branch feat-2

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -29,26 +29,26 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button Master',
+    label: 'Button Feat-2',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button Master',
+    label: 'Button Feat-2',
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-    label: 'Button Master',
+    label: 'Button Feat-2',
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    label: 'Button Master',
+    label: 'Button Feat-2',
   },
 };
